6b43391c044f705e02d96f5c7e6161111d770635
mariogilruiz <mariogilruiz66@gmail.com>
2023-10-21 02:45:22 +0200

    fin texto ramdon iinntial home

--- JS/script.js
+++ JS/script.js
@@ -15,6 +15,34 @@ $(function () {
 
 });
 
-// --------------------------
+// text ramdon
+
+document.addEventListener("DOMContentLoaded", function () {
+    const texts = document.querySelectorAll(".text");
+    let currentIndex = 0;
+
+    function rotateText() {
+        texts[currentIndex].classList.remove("visibility");
+        currentIndex = (currentIndex + 1) % texts.length;
+        texts[currentIndex].classList.add("visibility");
+    }
+
+    texts[currentIndex].classList.add("visibility");
+
+    setInterval(rotateText, 5000);
+});
 
 
+//h2 hidden
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elementToHide = document.getElementById("hideOnScroll");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 700) {
+            elementToHide.classList.add("hidden");
+        } else {
+            elementToHide.classList.remove("hidden");
+        }
+    });
+});
