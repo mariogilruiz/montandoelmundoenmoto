ff1c6afa971f3fe011428e795840af4b39cde9af
mariogilruiz <mariogilruiz66@gmail.com>
2023-10-22 19:15:05 +0200

    fin js nav_plus, empenzando con bttn_fuel

--- JS/script.js
+++ JS/script.js
@@ -79,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
 document.addEventListener("DOMContentLoaded", function () {
     const hiddenDivph = document.getElementById("content_ph");
 
@@ -99,30 +100,62 @@ window.addEventListener("scroll", function () {
     const bttn_sesion = document.getElementById("bttn_sesion");
     const bttn_h = document.getElementById("bttn_h");
     const bttn_p = document.getElementById("bttn_p");
+    const nav_plus = document.getElementById("nav_plus");
 
 
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > header.offsetHeight) {
+    if (scrollPosition > 30) {
         superpuesto.style.opacity = "0";
         header.classList.add("header_top");
+        // nav_plus.classList.add("header_top");
         logo_h.classList.add("header_opacity");
         bttn_home.classList.add("header_opacity");
         bttn_sesion.classList.add("header_opacity");
         bttn_h.classList.add("header_opacity");
         bttn_p.classList.add("header_opacity");
+        // nav_plus.classList.add("header_opacity");
 
     } else {
         superpuesto.style.opacity = "1";
         header.classList.remove("header_top");
+        // nav_plus.classList.remove("header_top");
         logo_h.classList.remove("header_opacity");
         bttn_home.classList.remove("header_opacity");
         bttn_sesion.classList.remove("header_opacity");
         bttn_h.classList.remove("header_opacity");
         bttn_p.classList.remove("header_opacity");
+        // nav_plus.classList.remove("header_opacity");
     }
 });
 
 
 //animation onclick menu plus
+
+document.getElementById("bttn_p").onclick = function () { nav_plus_hidden() };
+
+function nav_plus_hidden() {
+
+    document.getElementById("nav_plus").classList.toggle("hidden_nav_plus");
+    document.getElementById("bttn_p").classList.toggle("close");
+    document.getElementById("bttn_h").classList.toggle("hidden_bttn_h");
+    document.getElementById("bttn_p").classList.toggle("opacity_plus");
+    document.getElementById("logo_h").classList.toggle("opacity_plus");
+    document.getElementById("bttn_home").classList.toggle("hidden_bttn_header");
+    document.getElementById("bttn_sesion").classList.toggle("hidden_bttn_header");
+}
+
+document.getElementById("nav_chek").onclick = function () { bttn_fuel_hidden() };
+
+function bttn_fuel_hidden() {
+
+    document.getElementById("nav_chek").classList.toggle("hidden_bttn_fuel");
+
+
+
+
+}
+
+
+
 
