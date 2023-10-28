29387fc08361630d1bf918f8e2a801f526692d22
mariogilruiz <mariogilruiz66@gmail.com>
2023-10-28 15:19:38 +0200

    diseño sobre mi

--- JS/sobremi.js
+++ JS/sobremi.js
@@ -1,11 +1,11 @@
 //-------- efecto texto inicial de las pages comunes aparareciendo desde eje Y 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const hiddenDivph = document.getElementById("p_descript_la_serie_p");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const hiddenDivph = document.getElementById("p_descript_la_serie_p");
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 1) {
-            hiddenDivph.classList.add("apper_text_pages");
-        }
-    });
-});
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY > 1) {
+//             hiddenDivph.classList.add("apper_text_pages");
+//         }
+//     });
+// });
