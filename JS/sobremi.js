d6392933d24d65e40e6ed4d1db2cf47c40a39235
mariogilruiz <mariogilruiz66@gmail.com>
2023-10-29 15:56:44 +0100

    page proyecto

--- JS/sobremi.js
+++ JS/sobremi.js
@@ -1,11 +1,44 @@
-//-------- efecto texto inicial de las pages comunes aparareciendo desde eje Y 
+// -------- efecto texto inicial de las pages comunes aparareciendo desde eje Y 
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const hiddenDivph = document.getElementById("p_descript_la_serie_p");
+document.addEventListener("DOMContentLoaded", function () {
+    const hiddenDivph = document.getElementById("h4_content_02_about");
 
-//     window.addEventListener("scroll", function () {
-//         if (window.scrollY > 1) {
-//             hiddenDivph.classList.add("apper_text_pages");
-//         }
-//     });
-// });
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 170) {
+            hiddenDivph.classList.add("apper_text_pages");
+        }
+    });
+});
+
+//------ efecto aparacion de los div desde el eje Y
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hiddenDivph = document.getElementById("content_02_about_02");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            hiddenDivph.classList.add("appear_slow");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hiddenDivph = document.getElementById("content_02_about_03");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 800) {
+            hiddenDivph.classList.add("appear_slow");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hiddenDivph = document.getElementById("content_02_about_04");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 1200) {
+            hiddenDivph.classList.add("appear_slow");
+        }
+    });
+});
