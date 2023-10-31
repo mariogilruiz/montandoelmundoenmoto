012223849e1e3920646a3d01d23793b095ea7260
mariogilruiz <mariogilruiz66@gmail.com>
2023-10-31 15:51:11 +0100

    respaldo

---
+++ JS/geleria.js
@@ -0,0 +1,7 @@
+document.getElementById('selector_galery_ES').onclick = function () { card_galery_ES() };
+
+function card_galery_ES() {
+
+    document.getElementById('content_images_01_ES').classList.toggle("appear_map");
+
+}
