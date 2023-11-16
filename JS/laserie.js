eb0986da403c8b32edf9ad73bfa1beafa924dbfb
mariogilruiz <mariogilruiz66@gmail.com>
2023-11-16 17:54:52 +0100

    contador perfe

--- JS/laserie.js
+++ JS/laserie.js
@@ -95,3 +95,173 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+//contador de todo
+
+
+
+
+var countStarted = false;
+
+function countWhenClassAppears() {
+    if ($('#map_selection').hasClass('appear_map_selection')) {
+        if (!countStarted) {
+            // Contador hasta 71
+            var counter1 = { var: 0 };
+            var countTween1 = TweenMax.to(counter1, 5, {
+                var: 71,
+                onUpdate: function () {
+                    var number = Math.round(counter1.var);
+                    $('.counter1').html(number);
+                },
+                onComplete: function () {
+                    // Puedes agregar aquí cualquier lógica que desees cuando el contador 1 llega a 71
+                },
+                ease: Circ.easeOut
+            });
+
+            // Contador hasta 12000
+            var counter2 = { var: 0 };
+            var countTween2 = TweenMax.to(counter2, 5, {
+                var: 12000,
+                onUpdate: function () {
+                    var number = Math.round(counter2.var);
+                    $('.counter2').html(number);
+                },
+                onComplete: function () {
+                    // Puedes agregar aquí cualquier lógica que desees cuando el contador 2 llega a 12000
+                },
+                ease: Circ.easeOut
+            });
+
+            // Contador hasta 7
+            var counter3 = { var: 0 };
+            var countTween3 = TweenMax.to(counter3, 5, {
+                var: 7,
+                onUpdate: function () {
+                    var number = Math.round(counter3.var);
+                    $('.counter3').html(number);
+                },
+                onComplete: function () {
+                    // Puedes agregar aquí cualquier lógica que desees cuando el contador 3 llega a 7
+                },
+                ease: Circ.easeOut
+            });
+
+            countStarted = true; // Marca la bandera como true para indicar que se ha iniciado
+        }
+    } else {
+        $(window).on('scroll', function () {
+            var mapSelectionOffset = $('#map_selection').offset().top;
+            var windowScroll = $(window).scrollTop();
+
+            if (!countStarted && windowScroll + $(window).height() >= mapSelectionOffset) {
+                // Contador hasta 71
+                var counter1 = { var: 0 };
+                var countTween1 = TweenMax.to(counter1, 5, {
+                    var: 71,
+                    onUpdate: function () {
+                        var number = Math.round(counter1.var);
+                        $('.counter1').html(number);
+                    },
+                    onComplete: function () {
+                        // Puedes agregar aquí cualquier lógica que desees cuando el contador 1 llega a 71
+                    },
+                    ease: Circ.easeOut
+                });
+
+                // Contador hasta 12000
+                var counter2 = { var: 0 };
+                var countTween2 = TweenMax.to(counter2, 5, {
+                    var: 12000,
+                    onUpdate: function () {
+                        var number = Math.round(counter2.var);
+                        $('.counter2').html(number);
+                    },
+                    onComplete: function () {
+                        // Puedes agregar aquí cualquier lógica que desees cuando el contador 2 llega a 12000
+                    },
+                    ease: Circ.easeOut
+                });
+
+                // Contador hasta 7
+                var counter3 = { var: 0 };
+                var countTween3 = TweenMax.to(counter3, 5, {
+                    var: 7,
+                    onUpdate: function () {
+                        var number = Math.round(counter3.var);
+                        $('.counter3').html(number);
+                    },
+                    onComplete: function () {
+                        // Puedes agregar aquí cualquier lógica que desees cuando el contador 3 llega a 7
+                    },
+                    ease: Circ.easeOut
+                });
+
+                countStarted = true; // Marca la bandera como true para indicar que se ha iniciado
+                $(window).off('scroll');
+            }
+        });
+    }
+}
+
+// Llama a la función para comenzar el proceso
+$(document).ready(function () {
+    countWhenClassAppears();
+});
+
+
+
+
+//para tres contadores sin condiciones
+
+
+// function count() {
+//     var counter1 = { var: 0 };
+//     var counter2 = { var: 0 };
+//     var counter3 = { var: 0 };
+
+//     // Contador hasta 71
+//     var countTween1 = TweenMax.to(counter1, 5, {
+//         var: 71,
+//         delay: 0.8,
+//         onUpdate: function () {
+//             var number = Math.round(counter1.var);
+//             $('.counter1').html(number);
+//         },
+//         onComplete: function () {
+//             // Puedes agregar aquí cualquier lógica que desees cuando el primer contador llega a 71
+//         },
+//         ease: Circ.easeOut
+//     });
+
+//     // Contador hasta 12000
+//     var countTween2 = TweenMax.to(counter2, 5, {
+//         var: 12000,
+//         delay: 0.8,
+//         onUpdate: function () {
+//             var number = Math.round(counter2.var);
+//             $('.counter2').html(number);
+//         },
+//         onComplete: function () {
+//             // Puedes agregar aquí cualquier lógica que desees cuando el segundo contador llega a 12000
+//         },
+//         ease: Circ.easeOut
+//     });
+
+//     // Contador hasta 7
+//     var countTween3 = TweenMax.to(counter3, 5, {
+//         var: 7,
+//         delay: 0.8,
+//         onUpdate: function () {
+//             var number = Math.round(counter3.var);
+//             $('.counter3').html(number);
+//         },
+//         onComplete: function () {
+//             // Puedes agregar aquí cualquier lógica que desees cuando el tercer contador llega a 7
+//         },
+//         ease: Circ.easeOut
+//     });
+// }
+
+// count();
+
