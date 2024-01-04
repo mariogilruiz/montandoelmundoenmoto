7a558ca601750322b28dcd678560ad21095035b2
mariogilruiz <mariogilruiz66@gmail.com>
2024-01-04 19:20:08 +0100

    respaldo

--- JS/temporada1.js
+++ JS/temporada1.js
@@ -1,11 +1,18 @@
 //-------- efecto texto inicial de las pages comunes aparareciendo desde eje Y 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const hiddenDivph = document.getElementById("p_descript_la_serie_p");
+// text ramdon
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 1) {
-            hiddenDivph.classList.add("apper_text_pages");
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    const texts = document.querySelectorAll(".text");
+    let currentIndex = 0;
+
+    function rotateText() {
+        texts[currentIndex].classList.remove("visibility");
+        currentIndex = (currentIndex + 1) % texts.length;
+        texts[currentIndex].classList.add("visibility");
+    }
+
+    texts[currentIndex].classList.add("visibility");
+
+    setInterval(rotateText, 3000);
 });
